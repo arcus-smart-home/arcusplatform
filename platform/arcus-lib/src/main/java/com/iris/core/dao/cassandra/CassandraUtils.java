7258bbd6fbc4b7609f2ae7944a010db5e9d5ed7f
Andrew Sorensen <andrew@andrewsorensen.net>
2019-04-27 23:12:22 -0700

    Address More tab/space formatting issues

--- platform/arcus-lib/src/main/java/com/iris/core/dao/cassandra/CassandraUtils.java
+++ platform/arcus-lib/src/main/java/com/iris/core/dao/cassandra/CassandraUtils.java
@@ -21,39 +21,39 @@ import com.netflix.governator.configuration.KeyParser;
 
 public class CassandraUtils
 {
-	
-	public static <T> T getConfig(ConfigurationProvider config, ConfigurationKey key, Class<T> type, T dflt) {
-   	if(config.has(key)) {
-         if (String.class == type) {
-   		   return (T)config.getStringSupplier(key, (String)dflt).get();
-         } else {
-   		   return config.getObjectSupplier(key, dflt, type).get();
-   		}
-   	} else {
-   		return dflt;
-   	}
-	}
 
-	public static ConfigurationKey toKey(String key) {
-		return new ConfigurationKey(key, KeyParser.parse(key));
-	}
-   
-	public static ConfigurationKey toKey(String key, String name) {
-   	return toKey( String.format(key, name) );
-   }
+    public static <T> T getConfig(ConfigurationProvider config, ConfigurationKey key, Class<T> type, T dflt) {
+        if(config.has(key)) {
+            if (String.class == type) {
+                return (T)config.getStringSupplier(key, (String)dflt).get();
+            } else {
+                return config.getObjectSupplier(key, dflt, type).get();
+            }
+        } else {
+            return dflt;
+        }
+    }
 
-	public static ConfigurationKey toKey(String simpleProp, String namedProp, String name) {
-      return (name == null) ? toKey(simpleProp) : toKey(namedProp, name);
-   }
-	
-	public static String getUsername(ConfigurationProvider config, String name) {
-		ConfigurationKey propUser = toKey(CassandraConstants.CASSANDRA_USER_PROP, CassandraConstants.CASSANDRA_X_USER_PROP, name);
-		return getConfig(config, propUser, String.class, null);
-	}
-	
-	public static String getPassword(ConfigurationProvider config, String name) {
-		ConfigurationKey propPass = toKey(CassandraConstants.CASSANDRA_PASSWORD_PROP, CassandraConstants.CASSANDRA_X_PASSWORD_PROP, name);
-		return getConfig(config, propPass, String.class, null);
-	}
+    public static ConfigurationKey toKey(String key) {
+        return new ConfigurationKey(key, KeyParser.parse(key));
+    }
+
+    public static ConfigurationKey toKey(String key, String name) {
+        return toKey( String.format(key, name) );
+    }
+
+    public static ConfigurationKey toKey(String simpleProp, String namedProp, String name) {
+        return (name == null) ? toKey(simpleProp) : toKey(namedProp, name);
+    }
+
+    public static String getUsername(ConfigurationProvider config, String name) {
+        ConfigurationKey propUser = toKey(CassandraConstants.CASSANDRA_USER_PROP, CassandraConstants.CASSANDRA_X_USER_PROP, name);
+        return getConfig(config, propUser, String.class, null);
+    }
+
+    public static String getPassword(ConfigurationProvider config, String name) {
+        ConfigurationKey propPass = toKey(CassandraConstants.CASSANDRA_PASSWORD_PROP, CassandraConstants.CASSANDRA_X_PASSWORD_PROP, name);
+        return getConfig(config, propPass, String.class, null);
+    }
 }
 
