f899c4db6c228ac62a3243f0e8fb6378e9a13d96
Andrew Sorensen <andrew@andrewsorensen.net>
2019-09-08 12:14:33 -0700

    Add improved crypto + tests

--- platform/arcus-security/src/main/java/com/iris/security/crypto/AES.java
+++ platform/arcus-security/src/main/java/com/iris/security/crypto/AES.java
@@ -19,7 +19,9 @@ import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.GCMParameterSpec;
 
+import com.google.common.primitives.Bytes;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.crypto.hash.Md5Hash;
 import org.apache.shiro.crypto.hash.Sha256Hash;
@@ -29,6 +31,10 @@ import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import com.iris.Utils;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Arrays;
+
 /**
  * This class requires the Unlimited Strength Jurisdiction Policy installed.  See:
  * http://www.oracle.com/technetwork/java/javase/downloads/jce8-download-2133166.html
@@ -37,6 +43,7 @@ public class AES {
 
    private static final String KEY_ALG = "AES";
    private static final String CIPHER_ALG = "AES/CBC/PKCS5Padding";
+   private static final int GCM_IV_LENGTH = 12;
 
    @Inject
    @Named("iris.aes.secret")
@@ -46,34 +53,86 @@ public class AES {
    @Named("iris.aes.iv")
    private String iv;
 
+   private SecureRandom random;
+
    public AES() {
    }
 
-   public AES(String secret, String iv) {
+   public AES(String secret, String iv) throws NoSuchAlgorithmException {
       this.secret = secret;
       this.iv = iv;
+
+      this.random = SecureRandom.getInstanceStrong();
    }
 
    /**
     * @param key
     * @param value
-    * @return
-    *    Encrypted value base 64 encoded
+    * @return Encrypted value base64 encoded
     */
    public String encrypt(String key, String value) {
+      return encryptSafe(key, value);
+   }
+
+   /**
+    * This is the old version of the Arcus crypto implementation.
+    * AES CBC is subject to padding vulnerabilities and fallen into decline.
+    *
+    * TODO: this should be deleted, as we shouldn't encrypt this way anymore.
+    *
+    * @param key
+    * @param value
+    * @return
+    */
+   protected String encryptUnsafe(String key, String value) {
       Preconditions.checkNotNull(key, "key cannot be null");
-      if(StringUtils.isBlank(value)) {
+      if (StringUtils.isBlank(value)) {
          return null;
       }
 
       try {
          SecretKey secretKey = new SecretKeySpec(sha1(key, secret), KEY_ALG);
          IvParameterSpec ivParamSpec = new IvParameterSpec(md5(iv));
+
          Cipher cipher = Cipher.getInstance(CIPHER_ALG);
          cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivParamSpec);
+
          byte[] encrypted = cipher.doFinal(value.getBytes("UTF-8"));
          return Utils.b64Encode(encrypted);
-      } catch(Exception e) {
+      } catch (Exception e) {
+         throw new RuntimeException(e);
+      }
+   }
+
+   /**
+    * "Safe" encryptor that uses AES-GCM
+    *
+    * @param key
+    * @param value
+    * @return
+    */
+   protected String encryptSafe(String key, String value) {
+      Preconditions.checkNotNull(key, "key cannot be null");
+      if (StringUtils.isBlank(value)) {
+         return null;
+      }
+
+      try {
+         SecretKeySpec keySpec = new SecretKeySpec(sha1(key, secret), KEY_ALG);
+         byte[] iv = new byte[GCM_IV_LENGTH];
+         this.random.nextBytes(iv);
+
+         Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding", "BC");
+         IvParameterSpec ivParamSpec = new IvParameterSpec(iv);
+
+         cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
+
+         byte data[] = cipher.doFinal(value.getBytes("UTF-8"));
+
+         // Store the iv before the ciphertext
+         byte[] ciphertext = Bytes.concat(iv, data);
+         return Utils.b64Encode(ciphertext);
+      } catch (Exception e) {
          throw new RuntimeException(e);
       }
    }
@@ -84,8 +143,23 @@ public class AES {
     * @return
     */
    public String decrypt(String key, String value) {
+      try {
+         return decryptSafe(key, value);
+      } catch (Exception e) {
+         return decryptUnsafe(key, value);
+      }
+   }
+
+   /**
+    * Unsafe AES-CBC encryption
+    *
+    * @param key
+    * @param value
+    * @return
+    */
+   protected String decryptUnsafe(String key, String value) {
       Preconditions.checkNotNull(key, "key cannot be null");
-      if(StringUtils.isBlank(value)) {
+      if (StringUtils.isBlank(value)) {
          return null;
       }
 
@@ -96,11 +170,41 @@ public class AES {
          cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParamSpec);
          byte[] encrypted = cipher.doFinal(Utils.b64Decode(value));
          return new String(encrypted, "UTF-8");
-      } catch(Exception e) {
+      } catch (Exception e) {
          throw new RuntimeException(e);
       }
    }
 
+   /**
+    * Decrypt data encrypted with encryptSafe()
+    *
+    * @param key
+    * @param value
+    * @return
+    */
+   protected String decryptSafe(String key, String value) {
+      Preconditions.checkNotNull(key, "key cannot be null");
+      if (StringUtils.isBlank(value)) {
+         return null;
+      }
+
+      try {
+         byte[] decoded = Utils.b64Decode(value);
+
+         SecretKey secretKey = new SecretKeySpec(sha1(key, secret), KEY_ALG);
+         IvParameterSpec ivParamSpec = new IvParameterSpec(Arrays.copyOfRange(decoded, 0, GCM_IV_LENGTH));
+
+         Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding", "BC");
+         cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParamSpec);
+
+         byte[] plaintext = cipher.doFinal(Arrays.copyOfRange(decoded, GCM_IV_LENGTH, decoded.length));
+         return new String(plaintext, "UTF-8");
+      } catch (Exception e) {
+         throw new RuntimeException(e);
+      }
+   }
+
+   // XXX: why is this called sha1?
    private byte[] sha1(String key, String salt) throws Exception {
       return new Sha256Hash(key, salt).getBytes();
    }
@@ -109,4 +213,3 @@ public class AES {
       return new Md5Hash(str).getBytes();
    }
 }
-
---
+++ platform/arcus-security/src/test/java/com/iris/security/crypto/TestAES.java
@@ -0,0 +1,62 @@
+package com.iris.security.crypto;
+
+import com.iris.Utils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.security.Security;
+
+public class TestAES {
+    private AES aes;
+
+    @Before
+    public void setUp() throws Exception {
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+        aes = new AES("ylntN5dSPuTQoWga7TArBdlMhEibX+bG4h6sit2Jfq4=", "8e2DM7K/G2I=");
+    }
+
+    @Test
+    public void testAesCbc() {
+        String ctext = aes.encryptUnsafe("foo", "foo1");
+        String decoded = aes.unsafeDecrypt("foo", ctext);
+
+        Assert.assertEquals(decoded, "foo1");
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void testAesCbcManipulated() {
+        // thrown as java.lang.RuntimeException: javax.crypto.BadPaddingException:
+        thrown.expect(RuntimeException.class);
+        String ctext = aes.encryptUnsafe("foo", "foo1");
+        byte[] tamper = Utils.b64Decode(ctext);
+        tamper[3] = 'A';
+        String tampered = Utils.b64Encode(tamper);
+        String decoded = aes.decrypt("foo", tampered);
+    }
+
+    @Test
+    public void testAesGcm() {
+        String ctext = aes.encryptSafe("foo", "a very secret secret");
+        String decoded = aes.safeDecrypt("foo", ctext);
+
+        System.out.println("decrypted message: " + ctext);
+
+        Assert.assertEquals(decoded, "a very secret secret");
+    }
+
+    @Test
+    public void testAesGcmMessage() {
+        String ctext = aes.encryptSafe("foo", "a very secret secret");
+        String decoded = aes.safeDecrypt("foo", ctext);
+
+        System.out.println("decrypted message: " + ctext);
+
+        Assert.assertNotEquals(decoded, "not the same");
+    }
+}
