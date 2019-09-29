ac209f83f7d296254e583a15277cf5127f178aee
Andrew Sorensen <andrew@andrewsorensen.net>
2019-09-29 13:59:06 -0700

    Add noop address validator

---
+++ platform/arcus-containers/platform-services/src/main/java/com/iris/platform/address/validation/NoopAddressValidatorImpl.java
@@ -0,0 +1,16 @@
+package com.iris.platform.address.validation;
+
+import com.iris.messages.model.Place;
+import com.iris.platform.address.StreetAddress;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class NoopAddressValidatorImpl implements AddressValidator {
+    @Override
+    public AddressValidationResult validate(StreetAddress address) {
+        List<StreetAddress> addresses = new LinkedList<StreetAddress>();
+        addresses.add(address);
+        return new AddressValidationResult(addresses, true);
+    }
+}
