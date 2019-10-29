bb17b1550a5ae2460fef48f815a8ac2be3f7c34f
Andrew Sorensen <andrew@andrewsorensen.net>
2019-10-28 23:00:20 -0700

    Add more debugging

--- khakis/bin/release.sh
+++ khakis/bin/release.sh
@@ -12,9 +12,13 @@ REGISTRY_NAME=docker.pkg.github.com/$TRAVIS_REPO_SLUG
 
 echo "$GITHUB_SECRET" | docker login docker.pkg.github.com -u "$GITHUB_USERNAME" --password-stdin
 
+echo "Building and publishing containers to ${REGISTRY_SEPERATOR}"
+
 cd $ROOT/khakis
 $ROOT/khakis/bin/build.sh
+echo "tagging"
 $ROOT/khakis/bin/tag.sh
+echo "pushing"
 $ROOT/khakis/bin/push.sh
 cd -
 $GRADLE pushDocker
