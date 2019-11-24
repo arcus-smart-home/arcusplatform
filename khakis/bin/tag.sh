dc63565ad3756fcb3da08fba02af5781844d9f65
Andrew Sorensen <andrew@andrewsorensen.net>
2019-11-23 16:09:43 -0800

    Push -> Tag

--- khakis/bin/tag.sh
+++ khakis/bin/tag.sh
@@ -5,7 +5,7 @@ SCRIPT_PATH="$0"
 SCRIPT_DIR=$(dirname ${SCRIPT_PATH})
 . "${SCRIPT_DIR}/common.sh"
 
-# Push all of the images if none are specified
+# Tag all of the images if none are specified
 IMAGES="$@"
 if [ -z "${IMAGES}" ]; then
     IMAGES=""
