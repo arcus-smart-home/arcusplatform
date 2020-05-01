0b21542c5bed51e467236fc928696c6b13811128
Andrew Sorensen <andrew@andrewsorensen.net>
2020-04-30 20:06:42 -0700

    Reformat, bump copyright

--- platform/arcus-containers/notification-services/src/main/java/com/iris/notification/upstream/IrisUpstreamNotificationResponder.java
+++ platform/arcus-containers/notification-services/src/main/java/com/iris/notification/upstream/IrisUpstreamNotificationResponder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Arcus Project
+ * Copyright 2020 Arcus Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,87 +41,85 @@ import com.iris.platform.notification.audit.NotificationAuditor;
 @Singleton
 public class IrisUpstreamNotificationResponder implements UpstreamNotificationResponder {
 
-    private static final String ERROR_COUNT_SUFFIX = ".error.count";
+   private static final String ERROR_COUNT_SUFFIX = ".error.count";
    private static final IrisMetricSet METRICS = IrisMetrics.metrics(NotificationService.SERVICE_NAME);
-    private static final Logger logger = LoggerFactory.getLogger(IrisUpstreamNotificationResponder.class);
+   private static final Logger logger = LoggerFactory.getLogger(IrisUpstreamNotificationResponder.class);
 
-    @Inject
-    private MobileDeviceDAO mobileDeviceDao;
+   @Inject
+   private MobileDeviceDAO mobileDeviceDao;
 
-    private final NotificationAuditor auditor;
-    private final RetryProcessor retryProcessor;
-    private final Map<NotificationMethod,Counter>errorCounters;
+   private final NotificationAuditor auditor;
+   private final RetryProcessor retryProcessor;
+   private final Map<NotificationMethod, Counter> errorCounters;
 
-    @Inject
-    public IrisUpstreamNotificationResponder(NotificationAuditor auditor, RetryProcessor retryProcessor) {
-        this.auditor = auditor;
-        this.retryProcessor = retryProcessor;
-        
-        //Preinitialize the errors counters so we can see them in our tools even when errors haven't happened yet.
-        this.errorCounters=new HashMap<NotificationMethod,Counter>();
-        for(NotificationMethod method:NotificationMethod.values()){
-           Counter counter = METRICS.counter(method.toString().toLowerCase() + ERROR_COUNT_SUFFIX);
-           errorCounters.put(method, counter);
-        }
-    }
+   @Inject
+   public IrisUpstreamNotificationResponder(NotificationAuditor auditor, RetryProcessor retryProcessor) {
+      this.auditor = auditor;
+      this.retryProcessor = retryProcessor;
 
-    @Override
-    public void handleError(Notification notification, boolean isRecoverable, String message) {
-        logger.warn("Error sending notification {}:  {}", notification, message);
-        errorCounters.get(notification.getMethod()).inc();
-        if (isRecoverable) {
-            retryProcessor.retry(notification, new DispatchException(message));
-        } else {
-            auditor.log(notification, AuditEventState.ERROR, message);
-        }
-    }
-
-
-
-    @Override
-   public void handleHandOff(Notification notification) {
-       METRICS.counter(notification.getMethod().toString().toLowerCase() + ".handoff.count").inc();
-       auditor.log(notification, AuditEventState.ACCEPTED);
-       instrumentSuccessfulDispatch(notification.getMethod(), notification.getInflightDuration());
+      //Preinitialize the errors counters so we can see them in our tools even when errors haven't happened yet.
+      this.errorCounters = new HashMap<NotificationMethod, Counter>();
+      for (NotificationMethod method : NotificationMethod.values()) {
+         Counter counter = METRICS.counter(method.toString().toLowerCase() + ERROR_COUNT_SUFFIX);
+         errorCounters.put(method, counter);
+      }
    }
 
-    /**
-     * Increments the success counter associated with this notification method. Metric is named dispatch.{method}, i.e.,
-     * "notifications.dispatch.apns"
-     *
-     * @param method
-     *            The NotificationMethod by which the notification was dispatched.
-     */
-    protected void instrumentSuccessfulDispatch(NotificationMethod method, Duration inflightDuration) {
-        String counterName = "dispatch." + method.toString().toLowerCase();
-        String timerName = "dispatch." + method.toString().toLowerCase() + ".latency";
+   @Override
+   public void handleError(Notification notification, boolean isRecoverable, String message) {
+      logger.warn("Error sending notification {}:  {}", notification, message);
+      errorCounters.get(notification.getMethod()).inc();
+      if (isRecoverable) {
+         retryProcessor.retry(notification, new DispatchException(message));
+      } else {
+         auditor.log(notification, AuditEventState.ERROR, message);
+      }
+   }
 
-        IrisMetrics.metrics(NotificationService.SERVICE_NAME).counter(counterName).inc();
-        IrisMetrics.metrics(NotificationService.SERVICE_NAME).timer(timerName).update(inflightDuration.toMillis(), TimeUnit.MILLISECONDS);
-    }
 
    @Override
-    public void handleDeliveryReceipt(Notification notification) {
-        METRICS.counter(notification.getMethod().toString().toLowerCase() + ".deliveryreceipt.count").inc();
-        auditor.log(notification, AuditEventState.DELIVERED);
-    }
+   public void handleHandOff(Notification notification) {
+      METRICS.counter(notification.getMethod().toString().toLowerCase() + ".handoff.count").inc();
+      auditor.log(notification, AuditEventState.ACCEPTED);
+      instrumentSuccessfulDispatch(notification.getMethod(), notification.getInflightDuration());
+   }
 
-    @Override
-    public void handleDeviceUnregistered(NotificationMethod method, String deviceIdentifier) {
+   /**
+    * Increments the success counter associated with this notification method. Metric is named dispatch.{method}, i.e.,
+    * "notifications.dispatch.apns"
+    *
+    * @param method The NotificationMethod by which the notification was dispatched.
+    */
+   protected void instrumentSuccessfulDispatch(NotificationMethod method, Duration inflightDuration) {
+      String counterName = "dispatch." + method.toString().toLowerCase();
+      String timerName = "dispatch." + method.toString().toLowerCase() + ".latency";
 
-        METRICS.counter(method.toString().toLowerCase() + ".deviceexpired.count").inc();
+      IrisMetrics.metrics(NotificationService.SERVICE_NAME).counter(counterName).inc();
+      IrisMetrics.metrics(NotificationService.SERVICE_NAME).timer(timerName).update(inflightDuration.toMillis(), TimeUnit.MILLISECONDS);
+   }
 
-        // delete the device associated with this token/id
-        
-        // findWithToken will do a lookup in the mobile devices index table to 
-        // find the unique record matching the index
-        MobileDevice device = mobileDeviceDao.findWithToken(deviceIdentifier);
-        if (device != null) {
-           /*
-            * remove the device and the token to prevent further push notification attempts
-            */
-            mobileDeviceDao.delete(device);
-        }
-    }
+   @Override
+   public void handleDeliveryReceipt(Notification notification) {
+      METRICS.counter(notification.getMethod().toString().toLowerCase() + ".deliveryreceipt.count").inc();
+      auditor.log(notification, AuditEventState.DELIVERED);
+   }
+
+   @Override
+   public void handleDeviceUnregistered(NotificationMethod method, String deviceIdentifier) {
+
+      METRICS.counter(method.toString().toLowerCase() + ".deviceexpired.count").inc();
+
+      // delete the device associated with this token/id
+
+      // findWithToken will do a lookup in the mobile devices index table to
+      // find the unique record matching the index
+      MobileDevice device = mobileDeviceDao.findWithToken(deviceIdentifier);
+      if (device != null) {
+         /*
+          * remove the device and the token to prevent further push notification attempts
+          */
+         mobileDeviceDao.delete(device);
+      }
+   }
 }
 
