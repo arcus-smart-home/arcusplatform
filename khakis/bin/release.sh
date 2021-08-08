f8d9dd0ae4d2c98d59d15ba069e29b33a55a8b7a
Andrew Sorensen <andrew@andrewsorensen.net>
2021-08-07 22:28:55 -0700

    Update release script for github package registry

--- khakis/bin/release.sh
+++ khakis/bin/release.sh
@@ -3,16 +3,8 @@ set -e
 ROOT=$(git rev-parse --show-toplevel)
 GRADLE=$ROOT/gradlew
 
-if [[ "${TRAVIS_REPO_SLUG}" != 'wl-net/arcusplatform' ]]; then
+if [[ "${GITHUB_REPOSITORY}" != 'wl-net/arcusplatform' ]]; then
   exit 0  # skip due to not being on a known repo
-fi
-
-if [ -z ${DOCKERHUB_USER+x} ]; then
-  export REGISTRY_NAME=docker.pkg.github.com/$TRAVIS_REPO_SLUG
-  echo "$GITHUB_SECRET" | docker login docker.pkg.github.com -u "$GITHUB_USERNAME" --password-stdin
-else
-  export DOCKER_PREFIX_OVERRIDE=$DOCKERHUB_USER
-  echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USER" --password-stdin
 fi
 
 echo "Building and publishing containers to '${REGISTRY_NAME}'"
